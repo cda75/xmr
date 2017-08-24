2dabdf1f093e7daca4292dbcc5363d2fc49e8507
cda75 <dchestnov@mail.ru>
2017-08-24 10:24:06 +0300

    add debian

---
+++ build_debian.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get update
+apt-get install -y screen libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+git clone https://github.com/fireice-uk/xmr-stak-cpu.git .
+cmake -DCMAKE_LINK_STATIC=ON .
+make install
+cp config_xmr.txt bin/
+cp config_btc.txt bin/
+echo "vm.nr_hugepages=128" >> /etc/sysctl.conf
+sysctl -w vm.nr_hugepages=128
+echo "* soft memlock 262144" >> /etc/security/limits.conf
+echo "* hard memlock 262144" >> /etc/security/limits.conf
+cd bin/
+/usr/bin/screen -md -S xmr ./xmr-stak-cpu config_btc.txt
