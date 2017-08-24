f353ef14909b1f18a42431c10e1d23601bb75560
cda75 <dchestnov@mail.ru>
2017-08-24 10:28:59 +0300

    edit git dir

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 apt-get update
 apt-get install -y screen libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
-git clone https://github.com/fireice-uk/xmr-stak-cpu.git .
+git clone https://github.com/fireice-uk/xmr-stak-cpu xmr
+cd xmr
 cmake .
 make install
-cp config_xmr.txt bin/
-cp config_btc.txt bin/
+cp ../config_xmr.txt bin/
+cp ../config_btc.txt bin/
 echo "vm.nr_hugepages=128" >> /etc/sysctl.conf
 sysctl -w vm.nr_hugepages=128
 echo "* soft memlock 262144" >> /etc/security/limits.conf
